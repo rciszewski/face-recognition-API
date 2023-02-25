03bdec7346926d70acd4411ff9b197fb0f0c9a16
Ron <rc72095@icloud.com>
2023-02-25 18:44:05 -0500

    added connection to db on render

--- config.js
+++ config.js
@@ -2,4 +2,5 @@ const dotenv = require('dotenv');
 dotenv.config();
 module.exports = {
   API_KEY: process.env.API_KEY,
+  DB_KEY: process.env.DB_KEY
 };
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
+const { DB_KEY } = require('./config');
 const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
@@ -11,11 +12,11 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host : '127.0.0.1',
+    host : 'dpg-cfs0e8pgp3jqrleqogh0-a',
     port : 5432,
-    user : '',
-    password : '',
-    database : 'smart-brain'
+    user : 'smartbrain_qahb_user',
+    password : DB_KEY,
+    database : 'smartbrain_qahb'
   }
 });
 
@@ -25,7 +26,7 @@ app.use(express.json());
 app.use(cors());
 
 
-app.get('/', (req, res) => res.send('success'))
+app.get('/', (req, res) => res.send('success beautiful'))
 
 app.post('/signin', (req, res) => {signin.handleSignIn(req, res, bcrypt, db)})
 app.post('/register', (req, res) => {register.handleRegister(req, res, bcrypt, db)})
